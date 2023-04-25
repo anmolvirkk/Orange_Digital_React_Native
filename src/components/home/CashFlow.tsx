cc2b3e3d962c1f1b07537385138af7edbb75af7f
Anmol Virk <theanmolvirk@gmail.com>
2023-04-25 15:05:48 +1000

    update

--- src/components/home/CashFlow.tsx
+++ src/components/home/CashFlow.tsx
@@ -14,25 +14,6 @@ const CashFlow = () => {
 };
 
 const styles = StyleSheet.create({
-  card: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 12,
-    backgroundColor: "#ffffff",
-    borderBottomWidth: 1,
-    borderBottomColor: "#dcdcdc",
-    borderRadius: 5
-  },
-  details: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 4,
-    color: '#82919E',
-    textTransform: 'uppercase'
-  },
   container: {
     flex: 1,
     marginTop: 24,
@@ -41,15 +22,6 @@ const styles = StyleSheet.create({
     marginLeft: 12,
     marginRight: 12,
     borderRadius: 5
-  },
-  heading: {
-    fontSize: 22,
-    fontWeight: '500',
-    padding: 12,
-    paddingTop: 18,
-    paddingBottom: 18,
-    borderBottomWidth: 1,
-    borderBottomColor: "#dcdcdc"
   }
 });
 
